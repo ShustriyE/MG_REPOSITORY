3b371e72ef657854b731cbe7273245a805ca4d8d
ShustriyE <shutiyevgeniy@gmail.com>
2022-12-28 18:43:27 +0200

    addition switchCase

--- src/stores/PeopleStore.js
+++ src/stores/PeopleStore.js
@@ -17,11 +17,19 @@ export const usePeopleStore = defineStore('people', {
             this.people = JSON.parse(localStorage.getItem('people')) 
         },
         filterPeople(el) {
-            const result = el === 'film'
-            ? this.people.filter(person => person.films.length > 0)
-            : el === 'species'
-            ? this.people.filter(person => person.species.length > 0)
-            : this.people.filter(person => person.starships.length > 0)
+            let result;
+            switch(el) {
+                case 'film': 
+                result = this.people.filter(person => person.films.length > 0);
+                break;
+                case 'species':
+                result = this.people.filter(person => person.species.length > 0);
+                break;
+                case 'starships':
+                result = this.people.filter(person => person.starships.length > 0);
+                break;
+                default: break;
+            }
             this.setPeople(result) 
         },
     }
